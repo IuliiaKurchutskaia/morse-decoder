1b07c4e4a07b2d725e8ddca7e27b415df9b73f23
IuliiaKurchitskaia <53300963+IuliiaKurchutskaia@users.noreply.github.com>
2021-07-12 14:51:30 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,34 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    "**********": " "
 };
 
 function decode(expr) {
     // write your solution here
+   // let str1=expr.match(/.{10}/g);
+  //  for(let i=0; i<str1.length; i++){
+      //  str1[i].replace(/11/gi, "-");
+      //  str1[i].replace(/10/gi, ".");
+      //  str1[i].replace(/00/gi, "*");
+  //  }
+  //  return str1;
+
+  let decodedWord = "";
+
+  //words.map(item => {
+  for (let i = 0; i < expr.length; i += 10) {
+    decodedWord +=
+      MORSE_TABLE[
+        expr
+          .substr(i, 10)
+          .replace(/11/g, "-")
+          .replace(/10/g, ".")
+         .replace(/00/g, "")
+      ];
+  }
+
+  return decodedWord;
 }
 
 module.exports = {
